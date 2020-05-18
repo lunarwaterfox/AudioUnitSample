5f09becc430bd8edead402c84cdae885f6f6ea55
lunarwaterfox <lunarwaterfox@users.noreply.github.com>
2020-05-19 00:09:48 +0800

    add interface

---
+++ AudioUnitSample/PlayerAndRecorder/AudioUnitPlayer/AUPViewController.m
@@ -0,0 +1,48 @@
+//
+//  AUPViewController.m
+//  AudioUnitSample
+//
+//  Created by lagopus Alopex on 2020/5/18.
+//  Copyright © 2020 lagopus Alopex. All rights reserved.
+//
+
+#import "AUPViewController.h"
+
+@interface AUPViewController ()
+
+@end
+
+@implementation AUPViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)ready:(id)sender {
+    
+}
+
+- (IBAction)start:(id)sender {
+    
+}
+
+- (IBAction)pause:(id)sender {
+    
+}
+
+- (IBAction)stop:(id)sender {
+    
+}
+
+@end
